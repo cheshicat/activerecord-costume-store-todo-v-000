573b2113407c8b150799ac516b05cb8a605ff486
Allison Howard <alliemhow@gmail.com>
2019-01-10 10:12:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,11 +9,3 @@ class CreateCostumes < ActiveRecord::Migration
       t.updated_at
   end
 end
-
-name
-2. price
-3. size
-4. image url
-
-_as well as_ the two "timestamp" columns:  `created_at` and `updated_at`. This
-will provide a grand total of _six_ columns.
