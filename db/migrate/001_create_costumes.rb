2c6abcbb1a45955c1f6b4a436f8bb5aec90dd6d1
Allison Howard <alliemhow@gmail.com>
2019-01-10 10:12:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,10 +1,19 @@
 class CreateCostumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :price
+      t.string :size
+      t.string :image_url
+      t.created_at
+      t.updated_at
+  end
 end
 
-# By convention, the class name should match the part of the
-# file name after the number, so in this case:
-# 002_create_costumes.rb becomes class CreateCostumes
+name
+2. price
+3. size
+4. image url
 
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and
-# value types according to the spec
+_as well as_ the two "timestamp" columns:  `created_at` and `updated_at`. This
+will provide a grand total of _six_ columns.
