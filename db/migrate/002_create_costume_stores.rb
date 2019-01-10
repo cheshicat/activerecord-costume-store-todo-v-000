f6304e8774dec2b0e25527b1f1e1ed4ef10e05fd
Allison Howard <alliemhow@gmail.com>
2019-01-10 10:28:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,10 +8,6 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
-
-      6. opening time
-      7. closing time
     end
   end
 end
-# Create your costume_stores migration here
